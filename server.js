790722614ae990da2334fd51c66b912c40be1d89
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2021-09-22 09:57:22 -0400

    magic 8 ball working

--- server.js
+++ server.js
@@ -17,11 +17,7 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 const magicResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 app.get('/magic/:question', (req, res) => {
-    res.send(`<h1>${req.params.question}</h1> ${randomAnswer}`);
+    res.send(`<h1>${req.params.question}</h1> ${magicResponses[Math.floor(Math.random() * magicResponses.length)]}`);
 });
-
-function randomAnswer(magicResponses) {
-    return magicResponses[Math.floor(Math.random() * magicResponses.length)];
-};
 
 app.listen(3000);
