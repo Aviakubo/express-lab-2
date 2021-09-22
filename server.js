c575e7a01294b8b6a12a108d9538a30e36082c2f
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2021-09-21 22:45:14 -0400

    i've given up

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send('Your tip is $' + tipAmount)
 });
 
+const magicResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
+app.get('/magic/:question', (req, res) => {
+    res.send(`<h1>${req.params.question}</h1> ${randomAnswer}`);
+});
+
+function randomAnswer(magicResponses) {
+    return magicResponses[Math.floor(Math.random() * magicResponses.length)];
+};
 
 app.listen(3000);
